def5d2037354ccfb3424a292e9d5742954b039fc
Eternalll <43221983+EternalllZM@users.noreply.github.com>
2022-02-20 20:27:10 -0600

    Rewrite

--- Packager_OSX.sh
+++ Packager_OSX.sh
@@ -19,9 +19,9 @@ rm 'PackagerVersion_OSX.txt'
 rm 'GeckoVersion.txt'
 echo "The current versions of your Packager are:"
 echo " "
-cat '/Users/$USER/Library/Application\ Support/Dolphin/GameSettings/PackagerVersion_OSX.txt'
+cat './Sys/Resources/PackagerVersion_OSX.txt'
 echo " "
-cat '/Users/$USER/Library/Application\ Support/Dolphin/GameSettings/GeckoVersion.txt'
+cat './Sys/Resources/GeckoVersion.txt'
 echo " "
 echo " "
 echo "We will now update Packager, Update Gecko Codes, and Patch Dolphin in that order."
@@ -31,17 +31,18 @@ echo "If this is your first time running the script, you will not have local ver
 echo " "
 echo "You may stop the script at any time after each process if you do not want to perform the next step. You must run through all three steps your first time."
 read -p "Press Enter to update Packager"
+
 read -p "Press Enter again to confirm you read and understand the information above"
 echo "We are now updating Packager..."
 sleep 2
 curl 'https://raw.githubusercontent.com/EternalllZM/Packager/main/Sys/Resources/PackagerVersion_OSX.txt' -o 'PackagerVersion_OSX.txt'
-mv PackagerVersion_OSX.txt '/Users/$USER/Library/Application\ Support/Dolphin/GameSettings/'
+mv PackagerVersion_OSX.txt './Sys/Resources/'
 curl 'https://raw.githubusercontent.com/EternalllZM/Packager/main/Packager_OSX.sh' -o 'Packager_OSX.sh'
 echo "Packager was updated. If your local version reported differently, please quit and restart the script to receive the latest fixes and updates."
+cd '/Users/$USER/Library/Application Support/Dolphin/GameSettings/'
 read -p "Press Enter to update Gecko Codes"
 echo "Updating Gecko Codes..."
 sleep 1
-cd '/Users/$USER/Library/Application\ Support/Dolphin/GameSettings/'
 curl 'http://raw.githubusercontent.com/EternalllZM/Packager/main/User/Gamesettings/GKYE01.ini' -o 'GKYE01.ini'
 curl 'http://raw.githubusercontent.com/EternalllZM/Packager/main/User/Gamesettings/KHPE01.ini' -o 'KHPE01.ini'
 curl 'http://raw.githubusercontent.com/EternalllZM/Packager/main/User/Gamesettings/modifier_guide.txt' -o 'modifier_guide.txt'
